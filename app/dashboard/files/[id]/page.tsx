828d1525b2f32beafca14789bd1a03dcc219be9d
shekhar <shekharkubbireddy@gmail.com>
2025-06-18 13:13:57 +0530

    changed errors

--- app/dashboard/files/[id]/page.tsx
+++ app/dashboard/files/[id]/page.tsx
@@ -1,51 +1,102 @@
 
+// import PdfView from '@/components/PdfView';
+// import Chat from '@/components/Chat';
+// import { adminDb } from '@/firebaseAdmin'
+// import { auth } from '@clerk/nextjs/server'
+// import React from 'react'
+
+// const  ChatToFilePage = async (
+
+//     {params: {id}}: {params: {id:string}}
+    
+// ) => {
+
+//    auth.protect();
+//    const {userId} = await auth();
+
+//    if (!userId) {
+//      throw new Error("User ID is null");
+//    }
+
+//    const docId = id; // Assuming 'id' is the document ID passed in params
+//    const firebaseRef = await adminDb
+//    .collection("users")
+//    .doc(userId)
+//    .collection("files")
+//    .doc(docId)
+//    .get();
+
+//    const url = firebaseRef.data()?.downloadUrl; // Assuming the URL is stored in the document
+
+//   return (
+//     <div className='grid lg:grid-cols-5 h-full overflow-hidden'>
+//      {/* Right Part */}
+
+//      <div className='col-span-5 lg:col-span-2 overflow-y-auto'>
+//     {/* Chat */}
+
+//       <Chat id={id}/>
+//      </div>
+
+//      {/* Left part */}
+//      <div className='col-span-3 lg:col-span-3 bg-gray-100 border-r-2 lg:border-indigo-700 lg:-order-1 overflow-auto'>
+//       {/* PDFView */}
+//       <PdfView url={url}/>
+//      </div>
+
+//     </div>
+//   )
+// }
+
+// export default ChatToFilePage
+
+
+// app/dashboard/files/[id]/page.tsx
+
 import PdfView from '@/components/PdfView';
 import Chat from '@/components/Chat';
-import { adminDb } from '@/firebaseAdmin'
-import { auth } from '@clerk/nextjs/server'
-import React from 'react'
+import { adminDb } from '@/firebaseAdmin';
+import { auth } from '@clerk/nextjs/server';
+import React from 'react';
 
-const  ChatToFilePage = async (
-
-    {params: {id}}: {params: {id:string}}
-    
-) => {
-
-   auth.protect();
-   const {userId} = await auth();
-
-   if (!userId) {
-     throw new Error("User ID is null");
-   }
-
-   const docId = id; // Assuming 'id' is the document ID passed in params
-   const firebaseRef = await adminDb
-   .collection("users")
-   .doc(userId)
-   .collection("files")
-   .doc(docId)
-   .get();
-
-   const url = firebaseRef.data()?.downloadUrl; // Assuming the URL is stored in the document
-
-  return (
-    <div className='grid lg:grid-cols-5 h-full overflow-hidden'>
-     {/* Right Part */}
-
-     <div className='col-span-5 lg:col-span-2 overflow-y-auto'>
-    {/* Chat */}
-
-      <Chat id={id}/>
-     </div>
-
-     {/* Left part */}
-     <div className='col-span-3 lg:col-span-3 bg-gray-100 border-r-2 lg:border-indigo-700 lg:-order-1 overflow-auto'>
-      {/* PDFView */}
-      <PdfView url={url}/>
-     </div>
-
-    </div>
-  )
+interface PageProps {
+  params: {
+    id: string;
+  };
 }
 
-export default ChatToFilePage
+const ChatToFilePage = async ({ params }: PageProps) => {
+  auth.protect();
+  const { userId } = await auth();
+
+  if (!userId) {
+    throw new Error('User ID is null');
+  }
+
+  const docId = params.id;
+  const firebaseRef = await adminDb
+    .collection('users')
+    .doc(userId)
+    .collection('files')
+    .doc(docId)
+    .get();
+
+  const url = firebaseRef.data()?.downloadUrl;
+
+  return (
+    <div className="grid lg:grid-cols-5 h-full overflow-hidden">
+      {/* Right Part */}
+      <div className="col-span-5 lg:col-span-2 overflow-y-auto">
+        <Chat id={params.id} />
+      </div>
+
+      {/* Left part */}
+      <div className="col-span-3 lg:col-span-3 bg-gray-100 border-r-2 lg:border-indigo-700 lg:-order-1 overflow-auto">
+        <PdfView url={url} />
+      </div>
+    </div>
+  );
+};
+
+export default ChatToFilePage;
+
